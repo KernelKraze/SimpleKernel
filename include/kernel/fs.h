098804725a2817da3fe535ccc5ad65d441f442d6
geumbo <82752974+hello12324@users.noreply.github.com>
2024-05-27 22:49:03 +0900

    include: Update fs.h header file
    Updated the fs.h header file with placeholder comments for declaring file system routines.

---
+++ include/kernel/fs.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// File system header file for MyOS
+// Author: KernelKraze
+
+#ifndef FS_H
+#define FS_H
+
+// File system functions
+// Declare file system routines here
+
+#endif // FS_H
